e59e7df225df4476b643a0c81c86d6be1e3e03b0
Anxo Soto <anxo@glue.gl>
2017-07-24 10:07:39 +0200

    Added webpack.DefinePlugin to set basic globals on builds. Reorganized webpack config in a single file to avoid repetition.

---
+++ config/webpack.config.js
@@ -0,0 +1,70 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const webpack = require('webpack');
+
+const appDirectory = fs.realpathSync(process.cwd());
+
+module.exports = (isServerSide) => {
+
+  let config = {
+    name: isServerSide ? 'server' : 'client',
+    target: isServerSide ? 'node' : 'web',
+    output: {
+      path: path.resolve(
+        appDirectory, 'build', isServerSide ? 'server' : 'client'),
+      pathinfo: true,
+      filename: isServerSide ? 'server.js' : 'main.js',
+      publicPath: '/'
+    },
+    resolve: {
+      alias: {
+        'any-promise': 'core-js/fn/promise' // Prevents warning on webpack
+      },
+      extensions: ['.js', '.jsx', '.json'],
+      modules: [
+        path.resolve(__dirname, '..', 'node_modules'),
+        path.resolve(appDirectory, 'node_modules'),
+        appDirectory
+      ]
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin({ quiet: true }),
+      new webpack.NamedModulesPlugin(),
+      new webpack.DefinePlugin({
+        __DEV__: process.env.NODE_ENV === 'development',
+        __PROD__: process.env.NODE_ENV === 'production',
+        __SERVER__: isServerSide,
+        __CLIENT__: !isServerSide,
+      })
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: require.resolve('babel-loader'),
+          options: {
+            presets: [require.resolve('babel-preset-react-app')],
+            cacheDirectory: true,
+          }
+        }
+      ]
+    }
+  }
+
+  if (isServerSide) {
+    config.entry = [
+      path.resolve(__dirname, '..', 'server', 'lib', 'routerMiddleware')
+    ];
+  } else {
+    config.entry = [
+      path.resolve(appDirectory, 'src', 'index.js'),
+      'webpack-hot-middleware/client?name=client'
+    ];
+
+  }
+
+  return config;
+};
