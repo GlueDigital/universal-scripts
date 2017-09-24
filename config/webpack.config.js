b988d55574e082f7f70e90641c243deb6e4e8ac4
Anxo Soto <mancontr@gmail.com>
2017-09-24 20:14:03 +0200

    Added dotenv support.

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,5 +1,6 @@
 'use strict'
 
+require('dotenv').config()
 const fs = require('fs')
 const path = require('path')
 const webpack = require('webpack')
--- server/main.js
+++ server/main.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const chalk = require('chalk')
 const fs = require('fs')
 const Koa = require('koa')
