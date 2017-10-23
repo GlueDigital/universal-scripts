f747fdf270ae3f5dc0a16232815f7ff0250cce8e
Anxo Soto <anxo@glue.gl>
2017-10-23 13:43:51 +0200

    Removed warning for files >500Kb.

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -101,6 +101,7 @@ module.exports = (opts = {}) => {
           loader: require.resolve('babel-loader'),
           options: {
             presets: [require.resolve('babel-preset-react-app')],
+            compact: false,
             cacheDirectory: true
           }
         }, {
