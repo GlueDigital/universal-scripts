fa24608accac8ea41586c442ad09902f07032cb2
Anxo Soto <anxo@glue.gl>
2018-06-07 11:19:19 +0200

    Expose store through window.

--- client/init.js
+++ client/init.js
@@ -29,6 +29,13 @@ const initialState = window.___INITIAL_STATE__ || fakeInitialState()
 initialState.intl.messages = locales[initialState.intl.locale]
 const store = createStore(initialState)
 
+// We now expose the store through window
+// This is useful for debugging, and for some special cases where
+// the store is needed from outside react (ie: index.js, ...).
+// It should NOT be a substitute to connect or the other Redux utils.
+// Use with caution.
+window.store = store
+
 // Hook so user can add other locale data
 addLocaleData(localeData)
 
