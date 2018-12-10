e581efaedeb40805e41b7b57f0d69fe97f66aa2e
Anxo Soto <anxo@glue.gl>
2018-12-10 12:01:38 +0100

    Added lang change action to exports.

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 // Exports for user-code
+import langs from 'src/locales'
+import { updateIntl } from 'react-intl-redux'
+
 export const CLEANUP = '@@universal-scripts/cleanup'
 export const REQUEST_INIT = '@@universal-scripts/request-init'
 export const CLIENT_INIT = '@@universal-scripts/client-init'
+
+export const setLang = lang => updateIntl({
+  locale: lang,
+  messages: langs[lang]
+})
