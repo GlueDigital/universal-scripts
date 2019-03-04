f13da4dcf298e64854bcf10c4ad3e3d210bf4e98
Anxo Soto <anxo@glue.gl>
2019-03-04 17:50:30 +0100

    Updated dependencies, and base template.

--- template/src/routes/Home/Home.js
+++ template/src/routes/Home/Home.js
@@ -1,12 +1,8 @@
 import React from 'react'
 import { FormattedMessage } from 'react-intl'
 
-export class Home extends React.Component {
-  static displayName = 'Home'
-
-  render = () => {
-    return <h1><FormattedMessage id="home.welcome" /></h1>
-  }
+const Home = () => {
+  return <h1><FormattedMessage id="home.welcome" /></h1>
 }
 
 export default Home
