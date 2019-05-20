d2aac0c785b5c63150b85112102dbe0593339044
Anxo Soto <anxo@glue.gl>
2019-05-20 17:48:18 +0200

    Updated polyfills file to use core-js@3.

--- client/polyfills.js
+++ client/polyfills.js
@@ -1,11 +1,5 @@
 // Load some basic es6 polyfills
-import 'core-js/features/map'
-import 'core-js/features/set'
-import 'core-js/features/promise'
-import 'core-js/features/object/assign'
-import 'core-js/features/array/find-index'
-import 'core-js/features/array/find'
-import 'core-js/features/array/includes'
-import 'core-js/features/array/from'
-import 'core-js/features/symbol'
-import 'intl' // Required for IE10
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+// IE 10 requires intl polyfix too
+import 'intl'
