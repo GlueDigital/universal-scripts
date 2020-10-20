a21704539d01e7ce8a48c00a317f3ce83dc4a290
Anxo <mancontr@gmail.com>
2020-10-20 14:26:29 +0200

    Initial testing support

---
+++ scripts/test.js
@@ -0,0 +1,31 @@
+'use strict'
+process.env.NODE_ENV = 'test'
+
+process.on('unhandledRejection', err => { throw err })
+
+require('dotenv').config()
+
+const path = require('path')
+const fs = require('fs')
+const jest = require('jest')
+const argv = process.argv.slice(2)
+
+const config = {
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': path.resolve(__dirname, '../lib/jest/babelTransform')
+  },
+  transformIgnorePatterns: [
+    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$'
+  ],
+  setupFilesAfterEnv: []
+}
+
+const setupFiles = ['src/setupTests.ts', 'src/setupTests.js']
+setupFiles.forEach(f => {
+  if (fs.existsSync(f)) config.setupFilesAfterEnv.push('<rootDir>/' + f)
+})
+
+argv.push('--config', JSON.stringify(config))
+
+jest.run(argv)
