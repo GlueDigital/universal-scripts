5e9de8531365a9c8c6407f2af4e3d99f75b949a9
Anxo <mancontr@gmail.com>
2022-10-17 10:04:40 +0200

    Add ErrorHandler component option for ssr react error handling; fall back to client-side rendering on react errors

--- runtime.conf.d/server/50-render.js
+++ runtime.conf.d/server/50-render.js
@@ -1,6 +1,31 @@
+import React from 'react'
 import { renderToPipeableStream } from 'react-dom/server'
 
-// import { renderToStringAsync } from 'react-async-ssr'
+// Optional error 500 page
+const ErrorHandler = __SSR__ && (() => {
+  const req = require.context('src/routes', false, /^\.\/index$/)
+  const keys = req(req.keys()[0])
+  return keys.ErrorHandler
+})()
+
+const render = (ctx, root) => new Promise((resolve, reject) => {
+  const stream = renderToPipeableStream(root, {
+    bootstrapScripts: ctx.assets?.scripts,
+    onAllReady: () => {
+      if (ctx.renderCtx && ctx.renderCtx.url) {
+        // There was a redirect
+        ctx.redirect(ctx.renderCtx.url)
+      } else {
+        ctx.set('Content-Type', 'text/html; charset=utf-8')
+        ctx.stream = stream
+      }
+      resolve()
+    },
+    onError: (e) => {
+      reject(e)
+    }
+  })
+})
 
 const renderMiddleware = async (ctx, next) => {
   // Run any other middlewares
@@ -10,24 +35,16 @@ const renderMiddleware = async (ctx, next) => {
   const children = await ctx.triggerHook('reactRoot')(ctx, false)
 
   // Actual rendering
-  await new Promise((resolve, reject) => {
-    const stream = renderToPipeableStream(children, {
-      bootstrapScripts: ctx.assets?.scripts,
-      onAllReady: () => {
-        if (ctx.renderCtx && ctx.renderCtx.url) {
-          // There was a redirect
-          ctx.redirect(ctx.renderCtx.url)
-        } else {
-          ctx.set('Content-Type', 'text/html; charset=utf-8')
-          ctx.stream = stream
-        }
-        resolve()
-      },
-      onError: (e) => {
-        reject(e)
-      }
-    })
-  })
+  try {
+    await render(ctx, children)
+  } catch (err) {
+    if (ErrorHandler) {
+      await render(ctx, <ErrorHandler error={err} />)
+    } else {
+      await render(ctx, false)
+    }
+    ctx.status = err.status || 500
+  }
 }
 
 export const serverMiddleware = renderMiddleware
