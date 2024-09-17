034fd6ca2d440428c43bade3c7cf7181ef29cfc5
Alex Sanchez <alexs@glue.digital>
2024-09-17 16:43:16 +0200

    cut(deps): remove nodemon

--- bin/universal-scripts.js
+++ bin/universal-scripts.js
@@ -7,15 +7,6 @@ const spawn = require('cross-spawn')
 const script = process.argv[2]
 const args = process.argv.slice(3)
 
-if (script === 'start') {
-  const result = spawn.sync(
-    'nodemon',
-    [require.resolve('../scripts/' + script)].concat(args),
-    { stdio: 'inherit' }
-  )
-  process.exit(result.status)
-}
-
 const result = spawn.sync(
   'node',
   [require.resolve('../scripts/' + script)].concat(args),
