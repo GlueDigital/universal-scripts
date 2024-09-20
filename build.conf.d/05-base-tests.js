9744abfe5038c7afedec13aca2655b78af9f4a83
Alex Sanchez <alexs@glue.digital>
2024-09-20 16:42:48 +0200

    add(test): update jest config

--- build.conf.d/05-base-tests.js
+++ build.conf.d/05-base-tests.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 const enhancer = (opts = {}) => {
   const config = {
     roots: ['<rootDir>/src'],
-    moduleDirectories: ['node_modules', '.'],
+    moduleDirectories: ['node_modules', '<rootDir>'],
     transform: {
       '^.+\\.(js|jsx|ts|tsx)$': path.resolve(__dirname, '../lib/jest/babelTransform')
     },
@@ -12,8 +12,13 @@ const enhancer = (opts = {}) => {
       '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$'
     ],
     moduleNameMapper: {
-      '\\.(css|sass|scss)$': path.resolve(__dirname, '../lib/jest/emptyMock.js'),
-      '\\.(gif|jpg|jpeg|png|ttf|eot|svg)$': path.resolve(__dirname, '../lib/jest/emptyMock.js')
+      '\\.(css|sass|scss)$': path.resolve(__dirname, '../lib/jest/styleMock.js'),
+      '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': path.resolve(__dirname, '../lib/jest/fileMock.js'),
+      "^@components(.*)$": "<rootDir>/src/components$1",
+      "^@routes(.*)$": "<rootDir>/src/routes$1",
+      "^@static(.*)$": "<rootDir>/src/static$1",
+      "^@hooks(.*)$": "<rootDir>/src/hooks$1",
+      "^@utils(.*)$": "<rootDir>/src/utils$1",
     },
     setupFilesAfterEnv: []
   }
