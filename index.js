17dd06fa11353e480e4f5e1c65a7e12c52ba41d9
Alex Sanchez <alexs@glue.digital>
2024-11-27 16:17:00 +0100

    add(index): setLang function and set cookie

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+const locales = require('src/locales').default
+
 const { cleanup, clientInit } = require('./lib/redux/actions')
 const { updateIntl, requestInit } = require('./lib/redux/slices')
 
 const { useAppDispatch, useAppSelector } = require('./lib/redux/selector')
+
+
+const setLang = (lang, setCookie = true) => {
+  if (setCookie) document.cookie = `lang=${lang}`
+  updateIntl({
+    lang,
+    messages: locales[lang]
+  })
+}
 
 module.exports = {
   cleanup,
@@ -9,5 +20,6 @@ module.exports = {
   updateIntl,
   requestInit,
   useAppDispatch,
-  useAppSelector
+  useAppSelector,
+  setLang
 }
