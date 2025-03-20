495b818b841ddd0f255587749f0d2352f44dd625
Alex Sanchez <alexs@glue.digital>
2025-03-20 14:02:59 +0100

    cut(universal): remove config map

--- universal-scripts/config.js
+++ universal-scripts/config.js
@@ -37,13 +37,3 @@ export default async function getConfig(target) {
   )
   return config
 }
-
-const configMap = new Map()
-
-export function getConfigMap(key) {
-  return configMap.get(key)
-}
-
-export function setConfigMap(key, value) {
-  return configMap.set(key, value)
-}
