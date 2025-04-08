61aabdecd110ff894865d868f4de103100caa441
Alex Sanchez <alexs@glue.digital>
2025-04-08 12:25:37 +0200

    add(plugin-sass): import config from universal file

--- universal-plugins/universal-plugin-sass/build.conf.d/32-sass.js
+++ universal-plugins/universal-plugin-sass/build.conf.d/32-sass.js
@@ -1,20 +1,26 @@
 import { resolve } from 'path'
+import { getPluginsConfig } from 'universal-scripts/lib/universal-config.js'
 
 /**
- * @param {{ loaders: loader[], exts: string[] }} styleConfig [styleLoader, cssLoader, postCssLoader]
+ * @param {{ loaders: loader[], exts: string[] }} styleConfig [styleLoader, cssLoader, postCssLoader]...
  * @param {*} opts General opts for build
  * @returns updated loaders and extensions
  */
-export function stylesExtras(styleConfig, opts) {
+export async function stylesExtras(styleConfig, opts) {
   const [css] = styleConfig
   const { loaders } = css
+
+  const sassConfig = await getPluginsConfig('sass')
 
   const sassLoader = {
     loader: 'sass-loader',
     options: {
       sourceMap: true,
       sassOptions: {
-        includePaths: [resolve('appDirectory', 'src', 'styles')]
+        includePaths: [resolve('appDirectory', 'src', 'styles')],
+        quietDeps: true,
+        exclude: ['import'],
+        ...sassConfig
       }
     }
   }
